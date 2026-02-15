88ddecbd77bbcf83edafb1213e805f62466fa065
luis-fbs <baptistabaptista@discente.ufg.br>
2026-02-15 17:09:46 -0300

    Adjust file permissions

--- setup.sh
+++ setup.sh
@@ -5,9 +5,13 @@ for directory in ${DIRECTORIES[@]}; do
     fi
 done
 
+MOSQUITTO_UID="1883"
+MOSQUITTO_GID="1883"
+
 LOG_FILE="log/mosquitto.log"
 if [[ ! -f $LOG_FILE ]]; then
 	: > $LOG_FILE
+	sudo chown :$MOSQUITTO_GID $LOG_FILE
 fi
 
 PASSWD_FILE="config/passwd"
@@ -18,6 +22,6 @@ if [[ -f $PASSWD_FILE ]]; then
     fi
 else
     : > $PASSWD_FILE
-    sudo chown root:root $PASSWD_FILE
-    sudo chmod 700 $PASSWD_FILE
+    sudo chown $MOSQUITTO_UID:$MOSQUITTO_GID $PASSWD_FILE
+    sudo chmod 660 $PASSWD_FILE
 fi
